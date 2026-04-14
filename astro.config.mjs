78732b636d1d4a99c4685090702a50e86860b28b
Jess <jessgaspardev@gmail.com>
2026-04-14 14:51:15 +0100

    feat: add fonts API

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,18 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  fonts: [{ 
+    provider: fontProviders.fontsource(),
+    name: "Roboto",
+    cssVariable: "--font-roboto",
+    fallbacks: ["sans-serif"],
+    weights: [400, 500, 600, 700, 800, 900],
+    styles: ["normal", "italic"],
+  }],
   vite: {
     plugins: [tailwindcss()]
   }
