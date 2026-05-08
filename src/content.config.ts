fa4fb75c06bc690f9d088a50b65b641a4a63417d
yeico daza <kagemune.yd@gmail.com>
2026-05-08 15:19:14 -0500

    correcion de lenguajes manejados

--- src/content.config.ts
+++ src/content.config.ts
@@ -9,7 +9,7 @@ const projects = defineCollection({
         title: z.string().max(200),
         tools: z.preprocess(
             (val) => (Array.isArray(val) ? val : [val]),
-            z.array(z.enum(["TypeScript", "LIVE", "HTML", "JavaScript", "React Native", "Backend API", "Node.js", "Docker", "Tailwind"]))), // change categories here
+            z.array(z.enum(["TypeScript", "LIVE", "HTML", "JavaScript", "React Native", "Backend API", "Node.js", "Docker", "Tailwind","React", "Vite", "Google Sheets API", "AppSheet",]))), // change categories here
         year: z.string().max(4),
         liveSite: z.url().optional(),
         github: z.url().optional(),
